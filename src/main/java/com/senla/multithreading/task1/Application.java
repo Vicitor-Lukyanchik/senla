cb066564c5a2b86eea23b3316f5c2a39cfc34fa3
Vicitor <victorlukyanchik15@gmail.com>
2022-01-26 13:39:27 +0300

    1task

--- src/main/java/com/senla/multithreading/task1/Application.java
+++ src/main/java/com/senla/multithreading/task1/Application.java
@@ -5,12 +5,33 @@ public class Application {
     public static void main(String[] args) {
         MyThread myThread = new MyThread();
         Thread thread = new Thread(myThread);
-        System.out.println("NEW");
+        System.out.println(thread.getState());
         thread.start();
+
+        //Run
+        System.out.println(thread.getState());
+
+        //Wait
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
         }
+        System.out.println(thread.getState());
         myThread.notifyThread();
+
+        //Blocked
+        System.out.println(thread.getState());
+        myThread.notifyThread();
+
+        //Timed-waiting
+        System.out.println(thread.getState());
+        myThread.notifyThread();
+
+        //Terminated
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+        }
+        System.out.println(thread.getState());
     }
 }
--- src/main/java/com/senla/multithreading/task1/MyThread.java
+++ src/main/java/com/senla/multithreading/task1/MyThread.java
@@ -1,35 +1,21 @@
 package com.senla.multithreading.task1;
 
-import java.util.Scanner;
-
 public class MyThread implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("RUNNABLE");
-
         synchronized (this) {
             try {
-                System.out.println("WAITING");
                 wait();
             } catch (InterruptedException e) {
             }
         }
-
         try {
-            System.out.println("TIMED_WAITING");
-            Thread.sleep(3000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
         }
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("BLOCKED");
-        scanner.nextLine();
-        scanner.close();
-
-        System.out.println("TERMINATED");
     }
-    
+
     public synchronized void notifyThread() {
         notifyAll();
     }
