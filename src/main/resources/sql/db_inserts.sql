2bb1fc66e444948f14b0c0071d6122aacb15ede1
Vicitor <victorlukyanchik15@gmail.com>
2022-02-07 14:42:21 +0300

    add creating schema

--- src/main/resources/sql/db_inserts.sql
+++ src/main/resources/sql/db_inserts.sql
@@ -1,61 +1,40 @@
+USE lukyanchik_task10;
 INSERT INTO product (maker, model, type)
-VALUES ('Asus', 'PC1', 'pc');
-INSERT INTO product (maker, model, type)
-VALUES ('Samsung', 'PC2', 'pc');
-INSERT INTO product (maker, model, type)
-VALUES ('Lenovo', 'PC3', 'pc');
-INSERT INTO product (maker, model, type)
-VALUES ('Lenovo', 'PC4', 'pc');
-INSERT INTO product (maker, model, type)
-VALUES ('Huawei', 'PC5', 'pc');
-INSERT INTO product (maker, model, type)
-VALUES ('Lenovo', 'PC6', 'pc');
-INSERT INTO product (maker, model, type)
-VALUES ('Samsung', 'PC7', 'pc');
+VALUES ('Asus', 'PC1', 'pc'),
+       ('Samsung', 'PC2', 'pc'),
+       ('Lenovo', 'PC3', 'pc'),
+       ('Lenovo', 'PC4', 'pc'),
+       ('Huawei', 'PC5', 'pc'),
+       ('Lenovo', 'PC6', 'pc'),
+       ('Samsung', 'PC7', 'pc');
 
 INSERT INTO product (maker, model, type)
-VALUES ('Samsung', 'PR1', 'printer');
-INSERT INTO product (maker, model, type)
-VALUES ('Xiaomi', 'PR2', 'printer');
-INSERT INTO product (maker, model, type)
-VALUES ('Huawei', 'PR3', 'printer');
+VALUES ('Samsung', 'PR1', 'printer'),
+       ('Xiaomi', 'PR2', 'printer'),
+       ('Huawei', 'PR3', 'printer');
 
 INSERT INTO product (maker, model, type)
-VALUES ('Samsung', 'LT1', 'laptop');
-INSERT INTO product (maker, model, type)
-VALUES ('Redmi', 'LT3', 'laptop');
-INSERT INTO product (maker, model, type)
-VALUES ('Mac', 'LT2', 'laptop');
-INSERT INTO product (maker, model, type)
-VALUES ('Mac', 'LT4', 'laptop');
+VALUES ('Samsung', 'LT1', 'laptop'),
+       ('Redmi', 'LT3', 'laptop'),
+       ('Mac', 'LT2', 'laptop'),
+       ('Mac', 'LT4', 'laptop');
 
 INSERT INTO pc (code, model, speed, ram, hd, cd, price)
-VALUES (1, 'PC1', 2660, 6000, 128, '4x', 1000);
-INSERT INTO pc (code, model, speed, ram, hd, cd, price)
-VALUES (2, 'PC2', 2440, 4000, 64, '4x', 800);
-INSERT INTO pc (code, model, speed, ram, hd, cd, price)
-VALUES (3, 'PC3', 2880, 8000, 256, '4x', 1300);
-INSERT INTO pc (code, model, speed, ram, hd, cd, price)
-VALUES (4, 'PC4', 880, 1400, 32, '4x', 400);
-INSERT INTO pc (code, model, speed, ram, hd, cd, price)
-VALUES (5, 'PC5', 880, 1400, 32, '12x', 550);
-INSERT INTO pc (code, model, speed, ram, hd, cd, price)
-VALUES (6, 'PC6', 1440, 1800, 64, '24x', 930);
-INSERT INTO pc (code, model, speed, ram, hd, cd, price)
-VALUES (7, 'PC7', 1440, 1400, 64, '24x', 870);
+VALUES (1, 'PC1', 2660, 6000, 128, '4x', 1000),
+       (2, 'PC2', 2440, 4000, 64, '4x', 800),
+       (3, 'PC3', 2880, 8000, 256, '4x', 1300),
+       (4, 'PC4', 880, 1400, 32, '4x', 400),
+       (5, 'PC5', 880, 1400, 32, '12x', 550),
+       (6, 'PC6', 1440, 1800, 64, '24x', 930),
+       (7, 'PC7', 1440, 1400, 64, '24x', 870);
 
 INSERT INTO printer (code, model, color, type, price)
-VALUES (1, 'PR1', 'n', 'Laser', 500);
-INSERT INTO printer (code, model, color, type, price)
-VALUES (2, 'PR2', 'y', 'Jet', 700);
-INSERT INTO printer (code, model, color, type, price)
-VALUES (3, 'PR3', 'y', 'Matrix', 900);
+VALUES (1, 'PR1', 'n', 'Laser', 500),
+       (2, 'PR2', 'y', 'Jet', 700),
+       (3, 'PR3', 'y', 'Matrix', 900);
 
 INSERT INTO laptop (code, model, speed, ram, hd, price, screen)
-VALUES (1, 'LT1', 1440, 6000, 64, 400, 5);
-INSERT INTO laptop (code, model, speed, ram, hd, price, screen)
-VALUES (2, 'LT2', 840, 4000, 32, 200, 3);
-INSERT INTO laptop (code, model, speed, ram, hd, price, screen)
-VALUES (3, 'LT3', 1640, 8000, 128, 800, 6);
-INSERT INTO laptop (code, model, speed, ram, hd, price, screen)
-VALUES (4, 'LT4', 1640, 12000, 128, 1400, 12);
+VALUES (1, 'LT1', 1440, 6000, 64, 400, 5),
+       (2, 'LT2', 840, 4000, 32, 200, 3),
+       (3, 'LT3', 1640, 8000, 128, 800, 6),
+       (4, 'LT4', 1640, 12000, 128, 1400, 12);
--- src/main/resources/sql/db_schema.sql
+++ src/main/resources/sql/db_schema.sql
@@ -1,3 +1,6 @@
+DROP SCHEMA IF EXISTS lukyanchik_task10;
+CREATE SCHEMA IF NOT EXISTS lukyanchik_task10 DEFAULT CHARACTER SET utf8;
+USE lukyanchik_task10;
 CREATE TABLE product(
                         maker VARCHAR(10) NOT NULL,
                         model VARCHAR(50) PRIMARY KEY NOT NULL,
@@ -7,30 +10,33 @@ CREATE TABLE product(
 
 CREATE TABLE pc(
                    code         int PRIMARY KEY NOT NULL,
-                   model VARCHAR(50) REFERENCES NOT NULL REFERENCES product(model),
+                   model VARCHAR(50) NOT NULL,
                    speed smallint NOT NULL,
                    ram smallint NOT NULL,
                    hd real NOT NULL,
                    cd VARCHAR(10) NOT NULL,
-                   price decimal
+                   price decimal,
+                   FOREIGN KEY (model) REFERENCES product(model)
 );
 
 CREATE TABLE printer(
                         code int PRIMARY KEY NOT NULL,
-                        model VARCHAR(50) NOT NULL REFERENCES product(model),
+                        model VARCHAR(50) NOT NULL,
                         color char(1) DEFAULT 'n',
                         type VARCHAR(10) NOT NULL,
                         price decimal,
                         CHECK (type IN('Laser','Jet','Matrix')),
-                        CHECK (color IN('y', 'n'))
+                        CHECK (color IN('y', 'n')),
+                        FOREIGN KEY (model) REFERENCES product(model)
 );
 
 CREATE TABLE laptop(
                        code         int PRIMARY KEY,
-                       model VARCHAR(50) NOT NULL  REFERENCES product(model),
+                       model VARCHAR(50) NOT NULL,
                        speed smallint NOT NULL,
                        ram smallint NOT NULL,
                        hd real NOT NULL,
                        price decimal,
-                       screen smallint NOT NULL
+                       screen smallint NOT NULL,
+                       FOREIGN KEY (model) REFERENCES product(model)
 );
