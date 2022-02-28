34cd29030b580e9bcbac65c90ff1579e76950a96
Vicitor <victorlukyanchik15@gmail.com>
2022-02-28 13:07:40 +0300

    add checkstyle

--- src/main/java/com/senla/hotel/ApplicationStarter.java
+++ src/main/java/com/senla/hotel/ApplicationStarter.java
@@ -3,11 +3,8 @@ package com.senla.hotel;
 import com.senla.hotel.infrastucture.Application;
 import com.senla.hotel.infrastucture.ApplicationContext;
 import com.senla.hotel.ui.MenuController;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.config.DefaultConfiguration;
 
 public class ApplicationStarter {
-
 
     public static void main(String[] args) {
         ApplicationContext context = Application.run("com.senla.hotel");
